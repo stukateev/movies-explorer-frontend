a0d1389086fda864fb1638109aaf4f8bd60e8a9c
Dmitry Stukateev <Dmitry.Stukateev@yandex.ru>
2023-08-08 01:45:25 +0300

    fix: Profile.js setEditedValue

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,5 @@
 import './Profile.css'
-import React from 'react';
+import React, {useContext} from 'react';
 import { CurrentUserContext } from '../../context/currentUserContext';
 import isEmail from 'validator/lib/isEmail';
 import { mainApi } from '../../utils/MainApi';
@@ -7,29 +7,41 @@ import { regExpName } from '../../utils/constants';
 import {Link} from "react-router-dom";
 
 function Profile(props) {
-    const { currentUser, setCurrentUser, logOut } = props;
+    const currentUser = useContext(CurrentUserContext).currentUser;
+    const { setCurrentUser, logOut } = props;
     const {name, email} = React.useContext(CurrentUserContext);
     const [newName, setNewName] = React.useState(name);
     const [newEmail, setNewEmail] = React.useState(email);
     const [nameError, setNameError] = React.useState(false);
     const [emailError, setEmailError] = React.useState(false);
-
+    const [editedValue, setEditedValue] = React.useState(false);
 
     function checkName(e) {
         const name = e.target.value
-        if (!regExpName.test(e.target.value)) {
+        if (!regExpName.test(name)) {
+            if(name!==currentUser.name){
+                setEditedValue(true)
+            } else {
+                setEditedValue(false)
+            }
             setNameError(false);
             setNewName(name)
+
         } else {
             setNameError(true);
         }
     }
-
     function checkEmail(e) {
         const email = e.target.value;
         if (isEmail(email)) {
+            if(email!==currentUser.email){
+                setEditedValue(true)
+            } else {
+                setEditedValue(false)
+            }
             setEmailError(false);
             setNewEmail(email);
+            setEditedValue(true)
         } else {
             setEmailError(true);
         }
@@ -37,11 +49,11 @@ function Profile(props) {
 
     function patchUser(e) {
         e.preventDefault();
-        console.log(newName, newEmail)
         mainApi.patchUser(newName, newEmail)
             .then(res => {
                 setCurrentUser(res);
                 console.log(res)
+                setEditedValue(false)
             })
             .catch(err => {
                 if (err === 'Ошибка 401') {
@@ -57,6 +69,7 @@ function Profile(props) {
         setNewEmail('');
         setNameError(false);
         setEmailError(false);
+        setEditedValue(false)
 
     }
 
@@ -90,7 +103,7 @@ function Profile(props) {
                     <button className='profile__user-info-edit profile__button button'
                             type='button'
                             onClick={patchUser}
-                            disabled={!((newName !== name || newEmail !== email) && emailError === false && nameError === false && newName !== '')}>Редактировать</button>
+                            disabled={!((newName !== name || newEmail !== email) && emailError === false && nameError === false && newName !== '' && editedValue)}>Редактировать</button>
                 </form>
                 <Link className='profile__exit profile__button button' onClick={signOut} to="/">Выйти из аккаунта</Link>
             </section>
